7c260cde1b880ed81a4d2f350c9cf152d8131e74
Siebe-Timmer <257328732+siebetimmer-aug@users.noreply.github.com>
2026-04-02 00:19:50 +0200

    Evaluator + Generator

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -1,15 +1,62 @@
 package nl.han.ica.icss.generator;
 
+import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.*;
+import nl.han.ica.icss.ast.selectors.*;
 
-import nl.han.ica.icss.ast.AST;
+import java.util.List;
 
 public class Generator {
 
 	public String generate(AST ast) {
-        return "";
-
-
+		return generateStylesheet(ast.root);
 	}
 
-	
+	private String generateStylesheet(Stylesheet stylesheet) {
+		StringBuilder stringBuilder = new StringBuilder();
+		for (ASTNode child : stylesheet.getChildren()) {
+			if (child instanceof Stylerule) {
+				stringBuilder.append(generateStylerule((Stylerule) child)).append("\n");
+			}
+		}
+		return stringBuilder.toString();
+	}
+
+	private String generateStylerule(Stylerule stylerule) {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(generateSelector(stylerule.selectors));
+		for (ASTNode child : stylerule.body) {
+			if (child instanceof Declaration) {
+				stringBuilder.append("  ").append(generateDeclaration((Declaration) child)).append("\n");
+			}
+		}
+		stringBuilder.append("}");
+		return stringBuilder.toString();
+	}
+
+	private String generateSelector(List<Selector> selectors) {
+		StringBuilder stringBuilder = new StringBuilder();
+		for (Selector selector : selectors) {
+			if (selector instanceof TagSelector) {
+				stringBuilder.append(((TagSelector) selector).tag);
+			} else if (selector instanceof ClassSelector) {
+				stringBuilder.append(((ClassSelector) selector).cls);
+			} else if (selector instanceof IdSelector) {
+				stringBuilder.append(((IdSelector) selector).id);
+			}
+		}
+		return stringBuilder.append(" {\n").toString();
+	}
+
+	private String generateDeclaration(Declaration declaration) {
+		return declaration.property.name + ": " + generateLiteral((Literal) declaration.expression) + ";";
+	}
+
+	private String generateLiteral(Literal literal) {
+		if (literal instanceof ColorLiteral)      return ((ColorLiteral) literal).value;
+		if (literal instanceof PixelLiteral)      return ((PixelLiteral) literal).value + "px";
+		if (literal instanceof PercentageLiteral) return ((PercentageLiteral) literal).value + "%";
+		if (literal instanceof ScalarLiteral)     return String.valueOf(((ScalarLiteral) literal).value);
+		return "";
+	}
 }
--- startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
+++ startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
@@ -1,30 +1,148 @@
 package nl.han.ica.icss.transforms;
 
-import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.*;
-import nl.han.ica.icss.ast.literals.PercentageLiteral;
-import nl.han.ica.icss.ast.literals.PixelLiteral;
-import nl.han.ica.icss.ast.literals.ScalarLiteral;
-import nl.han.ica.icss.ast.operations.AddOperation;
-import nl.han.ica.icss.ast.operations.MultiplyOperation;
-import nl.han.ica.icss.ast.operations.SubtractOperation;
+import nl.han.ica.icss.ast.literals.*;
+import nl.han.ica.icss.ast.operations.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Evaluator implements Transform {
 
-    private IHANLinkedList<HashMap<String, Literal>> variableValues;
+    private LinkedList<HashMap<String, Literal>> variableValues;
 
     public Evaluator() {
-        //variableValues = new HANLinkedList<>();
+        variableValues = new LinkedList<>();
     }
 
     @Override
     public void apply(AST ast) {
-        //variableValues = new HANLinkedList<>();
-
+        variableValues = new LinkedList<>();
+        applyStylesheet(ast.root);
     }
 
-    
+    private void applyStylesheet(Stylesheet stylesheet) {
+        variableValues.addFirst(new HashMap<>());
+        for (int i = 0; i < stylesheet.getChildren().size(); i++) {
+            ASTNode child = stylesheet.getChildren().get(i);
+            if (child instanceof VariableAssignment) {
+                applyVariableAssignment((VariableAssignment) child);
+                stylesheet.removeChild(child);
+                i--;
+            } else if (child instanceof Stylerule) {
+                applyStylerule((Stylerule) child);
+            }
+        }
+        variableValues.removeFirst();
+    }
+
+    private void applyStylerule(Stylerule stylerule) {
+        variableValues.addFirst(new HashMap<>());
+        applyBodyNodes(stylerule.body);
+        variableValues.removeFirst();
+    }
+
+    private void applyBodyNodes(ArrayList<ASTNode> body) {
+        for (int i = 0; i < body.size(); i++) {
+            ASTNode node = body.get(i);
+            if (node instanceof VariableAssignment) {
+                applyVariableAssignment((VariableAssignment) node);
+                body.remove(i);
+                i--;
+            } else if (node instanceof Declaration) {
+                applyDeclaration((Declaration) node);
+            } else if (node instanceof IfClause) {
+                List<ASTNode> replacement = applyIfClause((IfClause) node);
+                body.remove(i);
+                body.addAll(i, replacement);
+                i--;
+            }
+        }
+    }
+
+    private List<ASTNode> applyIfClause(IfClause ifClause) {
+        Literal condition = evaluateExpression(ifClause.conditionalExpression);
+        boolean isTrue = condition instanceof BoolLiteral && ((BoolLiteral) condition).value;
+
+        if (isTrue) {
+            applyBodyNodes(ifClause.body);
+            return ifClause.body;
+        } else if (ifClause.elseClause != null) {
+            applyBodyNodes(ifClause.elseClause.body);
+            return ifClause.elseClause.body;
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    private void applyDeclaration(Declaration declaration) {
+        declaration.expression = evaluateExpression(declaration.expression);
+    }
+
+    private void applyVariableAssignment(VariableAssignment assignment) {
+        Literal literal = evaluateExpression(assignment.expression);
+        variableValues.getFirst().put(assignment.name.name, literal);
+    }
+
+    private Literal evaluateExpression(Expression expression) {
+        if (expression instanceof Literal) {
+            return (Literal) expression;
+        } else if (expression instanceof VariableReference) {
+            return evaluateVariableReference((VariableReference) expression);
+        } else if (expression instanceof MultiplyOperation) {
+            return evaluateMultiplyOperation((MultiplyOperation) expression);
+        } else if (expression instanceof AddOperation) {
+            return evaluateAddSubtract((AddOperation) expression, true);
+        } else if (expression instanceof SubtractOperation) {
+            return evaluateAddSubtract((SubtractOperation) expression, false);
+        }
+        return null;
+    }
+
+    private Literal evaluateMultiplyOperation(MultiplyOperation operation) {
+        Literal left  = evaluateExpression(operation.lhs);
+        Literal right = evaluateExpression(operation.rhs);
+
+        // Zorg dat scalar altijd rechts staat voor makkelijkere afhandeling
+        if (left instanceof ScalarLiteral && !(right instanceof ScalarLiteral)) {
+            Literal temp = left;
+            left = right;
+            right = temp;
+        }
+
+        if (left instanceof ScalarLiteral && right instanceof ScalarLiteral) {
+            return new ScalarLiteral(((ScalarLiteral) left).value * ((ScalarLiteral) right).value);
+        } else if (left instanceof PixelLiteral && right instanceof ScalarLiteral) {
+            return new PixelLiteral(((PixelLiteral) left).value * ((ScalarLiteral) right).value);
+        } else if (left instanceof PercentageLiteral && right instanceof ScalarLiteral) {
+            return new PercentageLiteral(((PercentageLiteral) left).value * ((ScalarLiteral) right).value);
+        }
+        return null;
+    }
+
+    private Literal evaluateAddSubtract(Operation operation, boolean isAdd) {
+        Literal left  = evaluateExpression(operation.lhs);
+        Literal right = evaluateExpression(operation.rhs);
+        int sign = isAdd ? 1 : -1;
+
+        if (left instanceof PixelLiteral && right instanceof PixelLiteral) {
+            return new PixelLiteral(((PixelLiteral) left).value + sign * ((PixelLiteral) right).value);
+        } else if (left instanceof PercentageLiteral && right instanceof PercentageLiteral) {
+            return new PercentageLiteral(((PercentageLiteral) left).value + sign * ((PercentageLiteral) right).value);
+        } else if (left instanceof ScalarLiteral && right instanceof ScalarLiteral) {
+            return new ScalarLiteral(((ScalarLiteral) left).value + sign * ((ScalarLiteral) right).value);
+        }
+        return null;
+    }
+
+    private Literal evaluateVariableReference(VariableReference reference) {
+        for (HashMap<String, Literal> scope : variableValues) {
+            if (scope.containsKey(reference.name)) {
+                return scope.get(reference.name);
+            }
+        }
+        return null;
+    }
 }
